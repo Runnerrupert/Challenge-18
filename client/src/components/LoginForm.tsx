86c4de4b1031c7b30c952ede455b4ccb4e7e1268
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-10-15 13:11:35 -0600

    modify the loginForm Component to incorporate changes with ApolloServer and GraphQl

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
 
@@ -12,6 +14,13 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
   const [userFormData, setUserFormData] = useState<User>({ username: '', email: '', password: '', savedBooks: [] });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser] = useMutation(LOGIN_USER, {
+    onCompleted: (data) => {
+      const { token } = data.login;
+      Auth.login(token);
+    }
+  });
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -29,14 +38,7 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token } = await response.json();
-      Auth.login(token);
+      await loginUser({ variables: { email: userFormData.email, password: userFormData.password } })
     } catch (err) {
       console.error(err);
       setShowAlert(true);
