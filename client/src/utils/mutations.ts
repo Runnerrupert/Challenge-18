36bcf8ee6bc12eb5807f84a816b0c6ee81b159fb
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-10-15 11:37:46 -0600

    Add mutation graphql strings for each mutation in client-side mutations.ts

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,1 +1,59 @@
 import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser($input: UserInput!) {
+        createUser(input: $input) {
+            token
+            user {
+                _id
+                username
+                email    
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email    
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: ID!) {
+        saveBook(bookId: $bookId) {
+            _id
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link    
+            }
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: ID!) {
+        createUser(bookId: $bookId) {
+            _id
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link    
+            }
+        }
+    }
+`;
